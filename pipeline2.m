073a3cbb017045254379654b32b09e7be0ba588d
yanxianUCSB <yanxian@umail.ucsb.edu>
2016-07-07 13:50:42 -0700

    AddedCombineSpectra
    created combine two cwEPR kinetic spectra

--- pipeline2.m
+++ pipeline2.m
@@ -3,8 +3,21 @@ if ~exist('root', 'var')
     root = uigetdir('SPC file folder');
 end
 
-% spc2txt2(root);
+nscancombine = 8;
+selection = [1 4 8];
+
+spc2txt2(root);
+
 title = input('Title = ', 's');
+
 track3peaks(root, title);
+
+track2Integral(root, title, nscancombine, selection)
+
+trackLineshape(root, title, nscancombine, selection)
+
+trackLineshapeNorm(root, title, nscancombine, selection)
+
+
 
 end
