c7fc51eb2238a7548a3a674936c32810213d04cd
yanxianUCSB <yanxian@umail.ucsb.edu>
2020-02-01 14:39:40 -0800

    MOVE helper to arv

--- helper/get_before_after.m
+++
@@ -1,9 +0,0 @@
-function [spc_before, spc_after] = get_before_after(SPC)
-assert(size(SPC, 2) > 2000);
-
-spc_before = mean(SPC(:, 1:1000), 2);
-spc_after = mean(SPC(:, end-1000:end), 2);
-% norm
-spc_before = (spc_before - min(spc_before))/range(spc_before);
-spc_after = (spc_after - min(spc_after))/range(spc_after);
-end
