7d97b985f73734150dbf217243bfa904e152b937
yanxianUCSB <yanxian@umail.ucsb.edu>
2020-02-01 14:33:03 -0800

    Created CWSpcArray object

--- demo.m
+++ demo.m
@@ -1,36 +1,44 @@
+%% setup
+addpath((fullfile(pwd, '3rd', 'easyspin-5.2.25', 'easyspin')));
+addpath((fullfile(pwd, '3rd', 'altmany-export_fig-eaeef72')));
 %% eprload
 FileName = 'data/real1D.spc';
 [x,y,Pars] = eprload(FileName);
 cwspc1d = CWSpc(FileName);
-%%
+
 FileName = 'data/real2D.spc';
 [x,y,Pars] = eprload(FileName);
 cwspc2d = CWSpc(FileName);
+
 %% 191223 overlapping multiple spectra
 clear all;
-pathin = "/Users/yanxlin/Box/data/CWEPR/191223-LLPS-salt-SL";
+clf;
+
+% path where to look for .spc files; make sure .spc and .par are in the
+% same path
+pathin = 'data/';
+
+% filenames without exstension
 FileNames = {
-    '313SL-NaCl-0'
-    '313SL-NaCl-1p5'
-    '313SL-NaCl-3p0'
-    '313SL-NaCl-4p5'
+    'real1D'
+    'real2D'
 };
+
+% cell of legends
 Legends = FileNames;
-cwsarray = CWSpc().empty();
-for ii = 1:numel(FileNames)
-    cwsarray(ii) = CWSpc(char(strcat(pathin, filesep, FileNames{ii}, '.spc')));
-    if cwsarray(ii).is1d
-        cwsarray(ii) = cwsarray(ii).mean();
-    else
-        cwsarray(ii) = cwsarray(ii).rmzeros().rmbadscans().mean();
-    end
-end
-[f, h] = CWSpc.stackplot(cwsarray, Legends);
+
+% create an array of CWSpc
+cwsarray = CWSpcArray(FileNames, pathin);
+
+% call stackplot with legends
+[f, h] = cwsarray.stackplot(Legends);
+
+% export figure
 % export_fig(char(strcat(pathin, filesep, '313SL-NaCl', '.pdf')))
 %%
 cws = CWSpc(char(strcat(pathin, filesep, FileNames{4}, '.spc')));
 cws
-CWSpc.stackplot(cws.mean())
+% CWSpc.stackplot(cws.mean())
 cws.image()
 %% rmzeros
 cws = cws.rmzeros()
